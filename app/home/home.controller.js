22e9c08c9a00d8d575396699faa8705bae5db495
Pasha <pasha@eventsoft.com.ua>
2016-02-21 19:12:47 +0200

    added config

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -2,6 +2,30 @@
 
 angular.module('myApp.homeController', [])
     .controller('homeController', function ($scope, $rootScope) {
+        $scope.config = {
+            tilesCount: 80,
+            level1: {
+                tilesCount: 5,
+                gridSize: 7
+            },
+            level2: {
+                tilesCount: 10,
+                gridSize: 9
+            },
+            level3: {
+                tilesCount: 15,
+                gridSize: 9
+            },
+            level4: {
+                tilesCount: 20,
+                gridSize: 11
+            },
+            level5: {
+                tilesCount: 30,
+                gridSize: 11
+            }
+        }
+
         $scope.currentSize = 7;
         $scope.currentLevel = 1;
 
@@ -12,7 +36,7 @@ angular.module('myApp.homeController', [])
                  $scope.foundTiles++;
                  checkState();
              }
-             else
+             else if ($scope.foundTiles <= 0)
                  $scope.foundTiles--;
 
             $rootScope.createGrid($scope.currentSize, 64, $scope.currentLevel);
@@ -20,11 +44,30 @@ angular.module('myApp.homeController', [])
 
          function checkState()
          {
-             if ($scope.foundTiles == 5)
-                 $scope.currentLevel++;
-             if ($scope.foundTiles == 10) {
-                 $scope.currentSize = 9;
-                 $scope.currentLevel++;
+             switch ($scope.foundTiles) {
+                 case $scope.config.level1.tilesCount:
+                     $scope.currentSize = $scope.config.level1.gridSize;
+                     $scope.currentLevel++;
+                     break;
+                 case $scope.config.level2.tilesCount:
+                     $scope.currentSize = $scope.config.level2.gridSize;
+                     $scope.currentLevel++;
+                     break;
+                 case $scope.config.level3.tilesCount:
+                     $scope.currentSize = $scope.config.level3.gridSize;
+                     $scope.currentLevel++;
+                     break;
+                 case $scope.config.level4.tilesCount:
+                     $scope.currentSize = $scope.config.level4.gridSize;
+                     $scope.currentLevel++;
+                     break;
+                 case $scope.config.level5.tilesCount:
+                     $scope.currentSize = $scope.config.level5.gridSize;
+                     $scope.currentLevel++;
+                     break;
+                 case $scope.config.tilesCount:
+                     alert("Win");
+                     break;
              }
          }
     });
